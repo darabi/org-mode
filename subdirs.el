3b727f582036d2ac46107c33480a049875fdac29
Kambiz Darabi <darabi@m-creations.net>
2015-05-04 21:35:50 +0200

    Add hu.dwim.*/emacs dirs to load-path

--- subdirs.el
+++ subdirs.el
@@ -4,3 +4,6 @@
 (push (expand-file-name "org-mode/lisp") load-path)
 
 (push (expand-file-name "hu.dwim.environment/emacs") load-path)
+(push (expand-file-name "hu.dwim.logger/emacs") load-path)
+(push (expand-file-name "hu.dwim.quasi-quote/emacs") load-path)
+(push (expand-file-name "hu.dwim.syntax-sugar/emacs") load-path)
