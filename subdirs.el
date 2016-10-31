2ea77fff286dcb11eceb58801a2bcecc24826816
Kambiz Darabi <darabi@m-creations.net>
2016-10-31 23:11:03 +0100

    New magit has a 'lisp/' subdir

--- subdirs.el
+++ subdirs.el
@@ -3,6 +3,8 @@
 
 (push (expand-file-name "org-mode/lisp") load-path)
 
+(push (expand-file-name "magit/lisp") load-path)
+
 (push (expand-file-name "hu.dwim.environment/emacs") load-path)
 (push (expand-file-name "hu.dwim.logger/emacs") load-path)
 (push (expand-file-name "hu.dwim.quasi-quote/emacs") load-path)
