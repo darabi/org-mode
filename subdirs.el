f5e8ee359e2c605f7740c9af8c508a03220e058b
Kambiz Darabi <darabi@m-creations.net>
2016-12-07 17:47:43 +0100

    Add bbdb/lisp to subdirs.el

--- subdirs.el
+++ subdirs.el
@@ -5,6 +5,8 @@
 
 (push (expand-file-name "magit/lisp") load-path)
 
+(push (expand-file-name "bbdb/lisp") load-path)
+
 (push (expand-file-name "hu.dwim.environment/emacs") load-path)
 (push (expand-file-name "hu.dwim.logger/emacs") load-path)
 (push (expand-file-name "hu.dwim.quasi-quote/emacs") load-path)
