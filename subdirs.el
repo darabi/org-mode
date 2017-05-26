e5d6672ee2bbde347a6ccdf5cc350c38cf9b8003
Kambiz Darabi <darabi@m-creations.net>
2017-05-26 09:54:20 +0200

    Add info subdirs of bbdb, org-mode, and magit

--- subdirs.el
+++ subdirs.el
@@ -2,10 +2,13 @@
     (normal-top-level-add-subdirs-to-load-path))
 
 (push (expand-file-name "org-mode/lisp") load-path)
+(add-to-list 'Info-default-directory-list (expand-file-name "org-mode/doc"))
 
 (push (expand-file-name "magit/lisp") load-path)
+(add-to-list 'Info-default-directory-list (expand-file-name "magit/Documentation"))
 
 (push (expand-file-name "bbdb/lisp") load-path)
+(add-to-list 'Info-default-directory-list (expand-file-name "bbdb/doc"))
 
 (push (expand-file-name "hu.dwim.environment/emacs") load-path)
 (push (expand-file-name "hu.dwim.logger/emacs") load-path)
