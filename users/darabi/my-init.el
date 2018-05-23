3c7072edfc300178b156a7c1b28df0fa6f6cba2c
Kambiz Darabi <darabi@m-creations.net>
2018-05-23 11:52:24 +0200

    users/darabi: use tide-mode for typescript

--- users/darabi/my-init.el
+++ users/darabi/my-init.el
@@ -1374,4 +1374,28 @@ Content-Type: text/plain; charset=utf-8")
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
 
 
+;; Typescript/Tide
+
+(defun setup-tide-mode ()
+  (interactive)
+  (require 'company)
+  (require 'tide)
+  (tide-setup)
+  (flycheck-mode +1)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  (eldoc-mode +1)
+  (tide-hl-identifier-mode +1)
+  ;; company is an optional dependency. You have to
+  ;; install it separately via package-install
+  ;; `M-x package-install [ret] company`
+  (company-mode +1))
+
+;; aligns annotation to the right hand side
+(setq company-tooltip-align-annotations t)
+
+;; formats the buffer before saving
+(add-hook 'before-save-hook 'tide-format-before-save)
+
+(add-hook 'typescript-mode-hook #'setup-tide-mode)
+
 (provide 'my-init)
