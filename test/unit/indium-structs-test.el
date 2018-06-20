75e8bd69b4e1fe5fb24749cb572b04369b0d3bf7
Kambiz Darabi <darabi@m-creations.net>
2018-06-20 17:06:21 +0200

    Squashed 'indium/' content from commit bc9ab60de
    git-subtree-dir: indium
    git-subtree-split: bc9ab60ded9af501fb4424359737cbff29cecfb1

---
+++ test/unit/indium-structs-test.el
@@ -0,0 +1,71 @@
+;;; indium-structs-test.el --- Unit tests for indium-structs.el  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2017-2018  Nicolas Petton
+
+;; Author: Nicolas Petton <nicolas@petton.fr>
+;; Keywords: test
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+(require 'buttercup)
+(require 'assess)
+(require 'indium-structs)
+(require 'cl-lib)
+
+(describe "Setting current connection slots"
+  (it "should be able to set the frames"
+    (with-indium-connection (make-indium-connection)
+      (setf (indium-current-connection-frames) 'foo)
+      (expect (indium-current-connection-frames)
+	      :to-be 'foo)))
+
+  (it "should be able to set the current frame"
+    (with-indium-connection (make-indium-connection)
+      (setf (indium-current-connection-current-frame) 'foo)
+      (expect (indium-current-connection-current-frame)
+	      :to-be 'foo))))
+
+(describe "Struct creation"
+  (it "should be able to make locations from script ids"
+    (spy-on 'indium-script-get-file :and-return-value "foo.js")
+    (spy-on 'indium-script-find-by-id :and-return-value "id")
+    (let ((loc (make-indium-location-from-script-id
+		:script-id "id"
+		:line 2
+		:column 3)))
+      (expect #'indium-script-find-by-id :to-have-been-called-with "id")
+      (expect (indium-location-file loc) :to-equal "foo.js")))
+
+  (it "Should be able to make breakpoints"
+    (let ((brk (make-indium-breakpoint
+		:id 'id
+		:line 5
+		:column 2
+		:file "foo.js")))
+      (expect (indium-breakpoint-id brk) :to-be 'id)
+      (expect (indium-location-file (indium-breakpoint-location brk))
+	      :to-equal "foo.js")
+      (expect (indium-location-line (indium-breakpoint-location brk))
+	      :to-equal 5)
+      (expect (indium-location-column (indium-breakpoint-location brk))
+	      :to-equal 2))))
+
+(provide 'indium-structs-test)
+;;; indium-structs-test.el ends here
